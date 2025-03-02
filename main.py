200e43de6960f057718910aee8e19f7fd1419c9c
MktR0 <mktronr@gmail.com>
2025-03-01 22:50:20 -0500

    update: add formatting to report output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from typing import final
 
 from stats import (
         get_word_count,
@@ -10,7 +11,8 @@ def main():
         book_path = sys.argv[1]
         text = get_book_text(book_path)
         report = prepare_report(book_path, text)
-        print_report(report, book_path)
+        final_report = generate_report(report, book_path)
+        print_report(final_report, book_path)
     except Exception:
         print("Usage: py(thon3) main.py <path_to_book>")
         sys.exit(1)
@@ -20,7 +22,7 @@ def get_book_text(path: str) -> str:
     with open(path,"r") as file:
         return file.read()
 
-def format_text(text: list) -> str:
+def format_input_text(text: list) -> str:
     formatted_text = "".join(text).lower()
     return formatted_text
 
@@ -29,23 +31,50 @@ def prepare_report(book_path: str, text: str):
     title = book_path.split("/")[1].capitalize()
     word_count: int = get_word_count(text)
     chars  = text.split()
-    char_count: dict = get_char_count(format_text(chars))
+    char_count: dict = get_char_count(format_input_text(chars))
     sorted_char_count = chars_dict_to_sorted_list(char_count)
     return title, word_count, sorted_char_count
 
-
-report_data = []
-def print_report(report, book_path):
+def generate_report(report, book_path):
+    final_report = []
     title, word_count, char_count = report
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book {title} found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
+    final_report.append("============ BOOKBOT ============")
+    final_report.append(f"Analyzing book {title} found at {book_path}...")
+    final_report.append("----------- Word Count ----------")
+    final_report.append(f"Found {word_count} total words")
+    final_report.append("--------- Character Count -------")
     for key in char_count.keys():
-        print(f"{key}: {char_count[key]}")
+        final_report.append(f"{key}: {char_count[key]}")
 
-    print("============= END ===============")
+    final_report.append("============= END ===============")
+
+    return final_report
+
+
+def print_report(final_report, book_path):
+
+    def format_output_text(line,length, format_type):
+        right_align = length - len(line)
+        left_align = len(line) - length
+        center_align = (length - len(line)) // 2
+
+        if format_type == "right":
+            return " " * right_align + line
+
+        if format_type == "left":
+            return " " * left_align + line
+
+        if format_type == "center":
+            return " "* center_align + line
+
+    title = final_report[1]
+    length = len(f"Analyzing book {title} found at {book_path}...") // 2
+    for i in range(len(final_report)):
+        if final_report[i]  == title:
+            print(title)
+            continue
+        line = format_output_text(final_report[i],length=length,format_type="center")
+        print(line)
 
 
 if __name__ == "__main__":
